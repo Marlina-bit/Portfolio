d30492cba851f448239d3cd148afa8d0e6372655
Marlina-bit <marjonbalmaceda15@gmail.com>
2025-04-13 11:35:54 +0800

    First Port

---
+++ Portfolio/portfolio.js
@@ -0,0 +1,24 @@
+
+
+    document.getElementById("home").addEventListener("click", function() {
+        window.location.href = "http://127.0.0.1:5500/home.html";
+    });
+
+    document.getElementById("about").addEventListener("click", function() {
+        window.location.href = "http://127.0.0.1:5500/Aboutme.html";
+    });
+
+    document.getElementById("skills").addEventListener("click", function() {
+        window.location.href = "http://127.0.0.1:5500/skills.html";
+    });
+
+    document.getElementById("project").addEventListener("click", function() {
+        window.location.href = "http://127.0.0.1:5500/project.html";
+    });
+
+    document.getElementById("resume").addEventListener("click", function() {
+        window.location.href = "http://127.0.0.1:5500/resume.html";
+    });
+
+   
+  
